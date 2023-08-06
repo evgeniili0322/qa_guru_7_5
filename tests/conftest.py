f49a924a270d4b1e7a150097e26d06dd809c669f
Evgenii Li <evgeniili2203@gmail.com>
2023-08-06 22:39:45 +0300

    Add --headless option

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ options = webdriver.ChromeOptions()
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_opt():
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
+    options.add_argument('window-size=1920,1080')
     browser.config.driver_options = options
     browser.config.base_url = 'https://demoqa.com'
 
