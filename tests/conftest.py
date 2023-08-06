28f937e4f02836bff89afd39ab82304dc226045d
Evgenii Li <evgeniili2203@gmail.com>
2023-08-06 20:28:38 +0300

    Add test steps

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,10 @@ options = webdriver.ChromeOptions()
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_opt():
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     browser.config.driver_options = options
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = 'https://demoqa.com'
+
+    yield
+
+    browser.quit()
